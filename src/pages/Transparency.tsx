3582f8625a794732c6d4396486fb84c17eff6626
Nathalia Cosim <nathalia.pcosim@gmail.com>
2023-12-05 08:41:21 -0400

    Agrupando entradas

--- src/pages/Transparency.tsx
+++ src/pages/Transparency.tsx
@@ -3,7 +3,6 @@ import getSaidas from "../services/SaidasService";
 import PageContainer from "../components/PageContainer";
 import Paper from "../components/Paper";
 import { useEffect, useState } from "react";
-import { AxiosResponse } from "axios";
 import "./Transparency.css";
 
 function Transparency() {
@@ -13,7 +12,7 @@ function Transparency() {
 
     const fetchEntradas = async () => {
         try {
-            const response: AxiosResponse<Entrada[], any> = await getEntradas();
+            const response = await getEntradas();
             if (response && response.data) {
                 setEntradas(response.data);
             } else {
@@ -29,7 +28,7 @@ function Transparency() {
 
     const fetchSaidas = async () => {
         try {
-            const responsex: AxiosResponse<Saida[], any> = await getSaidas();
+            const responsex = await getSaidas();
             if (responsex && responsex.data) {
                 setSaidas(responsex.data);
             } else {
@@ -43,6 +42,52 @@ function Transparency() {
         }
     };
 
+    const agruparPorNome = (lista: Entrada[]) => {
+    return lista.reduce<{
+        [key: string]: { somaValores: number; dataMaisRecente: Date; nomeDoador: string, quantidadeDoacoes: number };
+    }>((agrupado, item) => {
+        const chave = item.nomeDoador;
+
+        if (!agrupado[chave]) {
+            agrupado[chave] = {
+                somaValores: 0,
+                dataMaisRecente: new Date(0),
+                nomeDoador: item.nomeDoador,
+                quantidadeDoacoes: 0
+            };
+        }
+
+        agrupado[chave].somaValores += item.valor;
+
+        // Ajuste na conversão da string para Date
+        const dataItem = new Date(
+            item.dataHora.replace(/(\d{2})\/(\d{2})\/(\d{4}) (\d{2}:\d{2})/, '$3-$2-$1T$4:00')
+        );
+
+        if (dataItem.getTime() > agrupado[chave].dataMaisRecente.getTime()) {
+            agrupado[chave].dataMaisRecente = dataItem;
+        }
+
+        agrupado[chave].quantidadeDoacoes++;
+
+        return agrupado;
+    }, {} as { [key: string]: { somaValores: number; dataMaisRecente: Date; nomeDoador: string; quantidadeDoacoes: number } });
+};
+
+
+    const ordenarPorDataMaisRecente = (agrupado: {
+        [key: string]: {
+            somaValores: number;
+            dataMaisRecente: Date;
+            nomeDoador: string;
+            quantidadeDoacoes: number;
+        };
+    }) => {
+        return Object.values(agrupado).sort(
+            (a, b) => b.dataMaisRecente.getTime() - a.dataMaisRecente.getTime()
+        );
+    };
+
     useEffect(() => {
         fetchEntradas();
         fetchSaidas();
@@ -51,59 +96,87 @@ function Transparency() {
     return (
         <PageContainer>
             <Paper>
-                <div className="page-transparency">
                 <div>
                     <h1 className="entradas-title">Entradas</h1>
                     {error && <p>{error}</p>}
+                    <div className="page-transparency">
                         <table className="table-entradas">
                             <thead>
                                 <tr>
-                                    <th>Nome do Doador</th>
-                                    <th className="text-center data-hora">Data e Hora</th>
+                                    <th>Nome do Doador</th>                                    
+                                    <th className="text-center">
+                                       Qtd Doação | Última Doação
+                                    </th>
                                     <th className="text-right">Valor</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {Entradas.map((a) => (
-                                    <tr key={a._id}>
-                                        <td className="nome-doador">{a.nomeDoador}</td>
-                                        <td className="text-center data-hora">{a.dataHora}</td>
+                                {ordenarPorDataMaisRecente(
+                                    agruparPorNome(Entradas)
+                                ).map((agrupado) => (
+                                    <tr
+                                        key={
+                                            agrupado.nomeDoador
+                                        }
+                                    >
+                                        <td>{agrupado.nomeDoador}</td>                                        
+                                        <td className="text-center">
+                                           ({agrupado.quantidadeDoacoes}) {agrupado.dataMaisRecente.toLocaleString()}
+                                        </td>
                                         <td className="text-right">
-                                            {a.valor.toLocaleString("pt-BR", {
-                                                style: "currency",
-                                                currency: "BRL",
-                                            })}
+                                            {agrupado.somaValores.toLocaleString(
+                                                "pt-BR",
+                                                {
+                                                    style: "currency",
+                                                    currency: "BRL",
+                                                }
+                                            )}
                                         </td>
                                     </tr>
                                 ))}
                                 <tr className="tr-total">
-                                    <td className="font-bold no-border-right" colSpan={1}>
+                                    <td
+                                        className="font-bold no-border-right"
+                                        colSpan={2}
+                                    >
                                         Valor Total
                                     </td>
-                                    <td className="text-right font-bold no-border-left" colSpan={2}>
-                                        {Entradas.reduce(
-                                            (total, entrada) =>
-                                                total + entrada.valor,
-                                            0
-                                        ).toLocaleString("pt-BR", {
-                                            style: "currency",
-                                            currency: "BRL",
-                                        })}
+                                    <td
+                                        className="text-right font-bold no-border-left"
+                                        colSpan={2}
+                                    >
+                                        {ordenarPorDataMaisRecente(
+                                            agruparPorNome(Entradas)
+                                        )
+                                            .reduce(
+                                                (total, agrupado) =>
+                                                    total +
+                                                    agrupado.somaValores,
+                                                0
+                                            )
+                                            .toLocaleString("pt-BR", {
+                                                style: "currency",
+                                                currency: "BRL",
+                                            })}
                                     </td>
                                 </tr>
                             </tbody>
                         </table>
+                    </div>
                 </div>
                 <hr className="separator" />
                 <div>
                     <h1 className="saidas-title">Saídas</h1>
                     {error && <p>{error}</p>}
+                    <div className="page-transparency">
                         <table className="table-entradas">
                             <thead>
                                 <tr>
                                     <th>Destino</th>
                                     <th>Descrição</th>
-                                    <th className="text-center data-hora">Data e Hora</th>
+                                    <th className="text-center data-hora">
+                                        Data e Hora
+                                    </th>
                                     <th className="text-right">Valor</th>
                                 </tr>
                             </thead>
@@ -112,7 +185,9 @@ function Transparency() {
                                     <tr key={saida._id}>
                                         <td>{saida.destino}</td>
                                         <td>{saida.descricao}</td>
-                                        <td className="text-center data-hora">{saida.dataHora}</td>
+                                        <td className="text-center data-hora">
+                                            {saida.dataHora}
+                                        </td>
                                         <td className="text-right">
                                             {saida.valor.toLocaleString(
                                                 "pt-BR",
@@ -125,10 +200,16 @@ function Transparency() {
                                     </tr>
                                 ))}
                                 <tr className="tr-total">
-                                    <td className="font-bold no-border-right" colSpan={2}>
+                                    <td
+                                        className="font-bold no-border-right"
+                                        colSpan={2}
+                                    >
                                         Valor Total
                                     </td>
-                                    <td className="text-right font-bold no-border-left" colSpan={2}>
+                                    <td
+                                        className="text-right font-bold no-border-left"
+                                        colSpan={2}
+                                    >
                                         {Saidas.reduce(
                                             (total, saida) =>
                                                 total + saida.valor,
@@ -141,8 +222,8 @@ function Transparency() {
                                 </tr>
                             </tbody>
                         </table>
-                </div>
                     </div>
+                </div>
             </Paper>
         </PageContainer>
     );
