76ea23b728615bc11d7f3084b50a0805f87db69d
diogomene <diogodelimamenezes15@gmail.com>
2023-12-22 09:42:35 -0400

    Adding gallery

--- src/pages/Galeria.tsx
+++ src/pages/Galeria.tsx
@@ -1,32 +1,34 @@
 import PageContainer from "../components/PageContainer";
 import Paper from "../components/Paper";
 import fotos from "../../public/fotos.json";
-import './Galeria.css';
+import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+import "./Galeria.css";
 
-interface FotoUrl{
-  url_foto: string
+interface FotoUrl {
+    url_foto: string;
 }
 
 function Galeria() {
-   const fotosList : FotoUrl[] = fotos
-   return (
-    <PageContainer>
-      <Paper>
-        <div>
-          <h2>Galeria de Imagens</h2>
-          <div>
-            {
-              (fotosList).map((foto)=>{
-                return(
-                  <img className='fotos-evento' src={foto.url_foto}/>
-                )
-              })
-            }
-          </div>
-        </div>
-      </Paper>
-    </PageContainer>
-  );
+    const fotosList: FotoUrl[] = fotos;
+    const galleryPhotoList : ReactImageGalleryItem[] = fotosList.map((foto) => {
+        return {
+            original: foto.url_foto,
+            thumbnail: foto.url_foto
+        }
+    });
+    return (
+        <PageContainer>
+            <Paper>
+                <div className="image-gallery-page-container">
+                    <h2>Galeria de Imagens</h2>
+                    <div className="image-gallery-container">
+                        <ImageGallery items={galleryPhotoList} infinite={true} lazyLoad={true}/>
+                    </div>
+                </div>
+            </Paper>
+        </PageContainer>
+    );
 }
 
 export default Galeria;
